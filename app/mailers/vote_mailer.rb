62d36c8ca6362fbc3e1ad6ea33e4d91d7a903de3
Robert Philp <robert.h.philp@gmail.com>
2017-03-09 23:00:32 +0000

    Scaffolded mailer and added HTML and text views

---
+++ app/mailers/vote_mailer.rb
@@ -0,0 +1,11 @@
+class VoteMailer < ActionMailer::Base
+  default from: "from@example.com"
+
+  def vote_email(current_user, movie_user, type)
+    @current_user = current_user
+    @movie_user = movie_user
+    @type = type
+    @url  = 'http://example.com/login'
+    mail(to: @movie_user.email, subject: 'A vote has been cast')
+  end
+end
